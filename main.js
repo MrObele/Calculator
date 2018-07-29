b3ead83f156288b804545a5c6aefc1961f0ccb15
mrobele <obeledavid26@gmail.com>
2018-07-29 13:53:31 +0100

    Added the UI design of the calculator

--- main.js
+++ main.js
@@ -1,5 +1,60 @@
+// ===== Abasic JavaScript Calculator
+// ===== By Obele David
+
+
 function getKeyCode(e){
 	console.log(e);
 }
 
 window.addEventListener('keydown',getKeyCode);
+
+//functions for the basic operations
+
+function add(firstInput,secondInput){
+	if(!Number(firstInput) || !Number(secondInput)){
+		return;
+	}
+	console.log(Number(firstInput)+  Number(secondInput));
+}
+
+function subtract(firstInput,secondInput){
+	if(!Number(firstInput) || !Number(secondInput)){
+		return;
+	}
+	console.log(Number(firstInput) - Number(secondInput));
+}
+function multiply(firstInput,secondInput){
+	if(!Number(firstInput) || !Number(secondInput)){
+		return;
+	}
+	console.log(Number(firstInput) * Number(secondInput));
+}
+
+function divide(firstInput,secondInput){
+	if(!Number(firstInput) || !Number(secondInput)){
+		return;
+	}
+	console.log(Number(firstInput) /  Number(secondInput));
+}
+
+//function to perform the operation
+function operate(operand1, operand2, operatorType){
+	switch (operatorType){
+		case '+':
+		   console.log(add(operand1,operand2));
+		   break;
+		case '-':
+		   console.log(subtract(operand1,operand2));
+		   break;
+		case '*':
+		   console.log(multiply(operand1,operand2));
+		   break;
+		case '/':
+		   console.log(divide(operand1,operand2));
+		   break;
+		default:
+		console.log('Enter a valid arithmetic operator');            
+
+	}
+} 
+operate(2,'4','+');
